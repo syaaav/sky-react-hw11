dc6aa8dad5adff56f2d9d84fb12f79ba4327b6ba
syaaav <mysyava@gmail.com>
2022-07-21 18:52:54 +0300

    Environment setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import About from './components/pages/about/about'
+import SingIn from './components/pages/sing-in/sing-in'
+import GeneralCart from './components/pages/general-cart/general-cart'
+import Order from './components/pages/cart/order'
+import Page404 from './components/pages/page-404/page-404'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="app">
+    <Routes>
+      <Route path="/about" element={<About />} />
+      <Route path="/sing-in" element={<SingIn />} />
+      <Route path="/cart/:quantity/:price" element={<GeneralCart />} />
+      <Route path="/" element={<Order />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
+  </div>
+)
 
-export default App;
+export default App
